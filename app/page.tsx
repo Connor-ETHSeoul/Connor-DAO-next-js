4995af01e33e4e80c86a02e879664eee63694808
minseokkim <altpd13@gmail.com>
2024-03-30 00:56:47 +0900

    add main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,173 @@
-import Image from "next/image";
+'use client';
+
+import { LiveLogo } from '@/components/assets/LiveLogo';
+import { PaginationRight } from '@/components/assets/PaginationRight';
+import { PaginationLeft } from '@/components/assets/PaginatoinLeft';
+import { PollAmountLogo } from '@/components/assets/PollAmountLogo';
+import { PollDateRangeLogo } from '@/components/assets/PollDateRangeLogo';
+import PolicyStore from '@/store/policyStore';
+import Link from 'next/link';
+import { useSnapshot } from 'valtio';
 
 export default function Home() {
+  const { policyList } = useSnapshot(PolicyStore.state);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="bg-[#065CDE] bg-opacity-5">
+        <div className="livePolicyContainer bg-radial-gradient flex flex-col items-center gap-[45px] bg-opacity-30 py-[40px]">
+          <div className="livePolicy flex items-center gap-[13px]">
+            <LiveLogo className="h-[24px] w-[24px]"></LiveLogo>
+            <div className="text-[40px] font-semibold leading-10">
+              Live Policy
+            </div>
+          </div>
+          <div>
+            <ul className="policy flex items-start gap-[20px]">
+              {policyList.map((policy, idx) => (
+                <li key={idx} className="">
+                  <Link
+                    className="policyCard flex h-[383px] w-[339px] flex-col  items-start justify-between rounded-[20px] border-2 border-solid border-[#065CDE] bg-white p-[24px]"
+                    href={'/policy'}
+                  >
+                    <div className="policyTitle max-h-[156px] self-stretch text-[28px] font-semibold leading-[140%]">
+                      {policy.title}
+                    </div>
+                    <div className="policyCardBottom flex flex-col items-start gap-[14px] self-stretch">
+                      <div className="pollAndDate flex items-end justify-between self-stretch">
+                        <div className="text-[18px] font-semibold leading-[140%] text-[#065CDE]">
+                          60%
+                        </div>
+                        <div className="flex items-start gap-[10px] rounded-[43px] bg-[#065CDE] px-[8px] py-[4px] text-[16px] font-semibold leading-[140%] text-white">
+                          D-30
+                        </div>
+                      </div>
+                      <div className="pollBar flex h-[6px] items-center self-stretch bg-[#EEEFF0] pr-[145.5px]">
+                        <div className="pollBarYes h-[6px] w-[145.5px] bg-[#065CDE]"></div>
+                      </div>
+                      <div className="pollAmountAndDateRange flex flex-col items-start gap-[4px]">
+                        <div className="flex items-center gap-[10px]">
+                          <PollAmountLogo className="h-[16px] w-[16px]" />
+                          <div className="text-[16px] leading-[140%] text-gray-500">
+                            100,000
+                          </div>
+                        </div>
+                        <div className="flex items-center gap-[10px]">
+                          <PollDateRangeLogo className="h-[16px] w-[16px]" />
+                          <div className="text-[16px] leading-[140%] text-gray-500">
+                            YYYY.MM.DD-MM.DD
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="policyPagination flex items-start gap-[16px]">
+            <div className="flex h-[32px] w-[32px] flex-col items-center justify-center gap-[10px] p-[10px]">
+              <PaginationLeft className="h-[22px] w-[22px] shrink-0" />
+            </div>
+            <div className="paginationNumber flex items-start">
+              <div className="flex h-[32px] w-[32px] flex-col items-center justify-center gap-[10px] rounded-[10px] bg-black p-[10px] text-[16px] font-medium leading-[140%] text-white">
+                1
+              </div>
+            </div>
+            <div className="paginationNumber flex items-start">
+              <div className="flex h-[32px] w-[32px] flex-col items-center justify-center gap-[10px] rounded-[10px] p-[10px] text-[16px] font-medium leading-[140%] text-gray-600">
+                2
+              </div>
+            </div>
+            <div className="paginationNumber flex items-start">
+              <div className="flex h-[32px] w-[32px] flex-col items-center justify-center gap-[10px] rounded-[10px] p-[10px] text-[16px] font-medium leading-[140%] text-gray-600">
+                3
+              </div>
+            </div>
+            <div className="flex h-[32px] w-[32px] flex-col items-center justify-center gap-[10px] p-[10px]">
+              <PaginationRight className="h-[22px] w-[22px] shrink-0" />
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      {/* All Policies */}
+      <div className="allPolicyContainer mx-[8%] my-[50px] flex flex-col">
+        <div className="text-[28px] font-semibold leading-[140%]">
+          All Policies
+        </div>
+        <div className="allPolicyContainer flex flex-col items-start gap-[45px] py-[40px]">
+          <div>
+            <ul className="policy flex items-start gap-[20px]">
+              {policyList.map((policy, idx) => (
+                <li
+                  key={idx}
+                  className="policyCard flex h-[383px] w-[285px] flex-shrink-0 flex-col  items-start justify-between rounded-[20px] border border-solid border-[#DDE0E1] bg-white p-[20px]"
+                >
+                  <div className="policyTitle flex max-h-[156px] flex-col gap-[8px] self-stretch">
+                    <div className="text-[16px] font-medium leading-[100%] text-[#065CDE]">
+                      Voting inprogress
+                    </div>
+                    <div className="text-[22px] font-semibold leading-[140%]">
+                      {policy.title}
+                    </div>
+                  </div>
+                  <div className="policyCardBottom flex flex-col items-start gap-[14px] self-stretch">
+                    <div className="pollAndDate flex items-end justify-between self-stretch">
+                      <div className="text-[18px] font-semibold leading-[140%] text-[#065CDE]">
+                        60%
+                      </div>
+                      <div className="flex items-start gap-[10px] rounded-[43px] bg-[#065CDE] px-[8px] py-[4px] text-[16px] font-semibold leading-[140%] text-white">
+                        D-30
+                      </div>
+                    </div>
+                    <div className="pollBar flex h-[6px] items-center self-stretch bg-[#EEEFF0] pr-[145.5px]">
+                      <div className="pollBarYes h-[6px] w-[145.5px] bg-[#065CDE]"></div>
+                    </div>
+                    <div className="pollAmountAndDateRange flex flex-col items-start gap-[4px]">
+                      <div className="flex items-center gap-[10px]">
+                        <PollAmountLogo className="h-[16px] w-[16px]" />
+                        <div className="text-[16px] leading-[140%] text-gray-500">
+                          100,000
+                        </div>
+                      </div>
+                      <div className="flex items-center gap-[10px]">
+                        <PollDateRangeLogo className="h-[16px] w-[16px]" />
+                        <div className="text-[16px] leading-[140%] text-gray-500">
+                          YYYY.MM.DD-MM.DD
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <div className="flex justify-around">
+          <div className="policyPagination flex items-center gap-[16px]">
+            <div className="flex h-[32px] w-[32px] flex-col items-center justify-center gap-[10px] p-[10px]">
+              <PaginationLeft className="h-[22px] w-[22px] shrink-0" />
+            </div>
+            <div className="paginationNumber flex items-start">
+              <div className="flex h-[32px] w-[32px] flex-col items-center justify-center gap-[10px] rounded-[10px] bg-black p-[10px] text-[16px] font-medium leading-[140%] text-white">
+                1
+              </div>
+            </div>
+            <div className="paginationNumber flex items-start">
+              <div className="flex h-[32px] w-[32px] flex-col items-center justify-center gap-[10px] rounded-[10px] p-[10px] text-[16px] font-medium leading-[140%] text-gray-600">
+                2
+              </div>
+            </div>
+            <div className="paginationNumber flex items-start">
+              <div className="flex h-[32px] w-[32px] flex-col items-center justify-center gap-[10px] rounded-[10px] p-[10px] text-[16px] font-medium leading-[140%] text-gray-600">
+                3
+              </div>
+            </div>
+            <div className="flex h-[32px] w-[32px] flex-col items-center justify-center gap-[10px] p-[10px]">
+              <PaginationRight className="h-[22px] w-[22px] shrink-0" />
+            </div>
+          </div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
---
+++ store/policyStore.ts
@@ -0,0 +1,47 @@
+import { proxy } from 'valtio';
+
+export interface IPolicy {
+  policyId: number;
+  title: string;
+  description: string;
+  policy: string;
+  yesVoteAmount: number;
+  noVoteAmount: number;
+  abstainVoteAmount: number;
+  dueDate: string;
+}
+
+interface PolicyListState {
+  policyList: IPolicy[];
+}
+
+const state = proxy<PolicyListState>({
+  policyList: [
+    {
+      policyId: 0,
+      title: 'Title Name 1',
+      description: '## Hello World. This is Policy 1',
+      policy: 'Do not Attack Human',
+      yesVoteAmount: 60,
+      noVoteAmount: 30,
+      abstainVoteAmount: 10,
+      dueDate: '2024-04-01',
+    },
+  ],
+});
+
+const PolicyStore = {
+  state,
+  addPolicyList(newPolicyList: IPolicy[]) {
+    state.policyList = state.policyList.filter(
+      (policy) => !newPolicyList.includes(policy),
+    );
+    state.policyList = state.policyList.concat(newPolicyList);
+  },
+  deletePolicy(value: IPolicy) {
+    state.policyList = state.policyList.filter(
+      (policy) => policy.policyId !== value.policyId,
+    );
+  },
+};
+export default PolicyStore;
