3702f8c230a66b854aae824e0fd96087f5c5f32b
Joon2000 <najoonwon22@gmail.com>
2024-03-31 02:47:08 +0900

    add game logic

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -15,11 +15,57 @@ import stabbingButton from '@/public/btn_attack1.svg';
 import swingButton from '@/public/btn_attack2.svg';
 
 import Image from 'next/image';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import HumanConditionalRender from './components/HumanConditionalRender';
 
+import {
+  createWalletClient,
+  http,
+  getContract,
+  createPublicClient,
+  LocalAccount,
+} from 'viem';
+import { MPC_CONTRACT, NearWallet } from '@/utils/near-wallet';
+import { useSnapshot } from 'valtio';
+import WalletStore from '@/store/walletStore';
+import abi from '../contractJson/Game.json';
+import 'viem/window';
+import { sepolia } from 'viem/chains';
+
 export default function GamePage() {
-  const [selectedEnemy, setSelectedEnemy] = useState('oldMan');
+  const wallet = useMemo(
+    () => new NearWallet({ createAccessKeyFor: MPC_CONTRACT }),
+    [],
+  );
+  const { isSignedIn, accountId } = useSnapshot(WalletStore.state);
+
+  const SEPOLIA_RPC_URL =
+    'https://endpoints.omniatech.io/v1/eth/sepolia/public';
+  const gameAddress = '0x589685A025C0DE5f21904314d235ad093b3bFEc3';
+  const contractABI = abi.abi;
+
+  const publicClient = createPublicClient({
+    chain: sepolia,
+    transport: http(SEPOLIA_RPC_URL),
+  });
+  const client = createWalletClient({
+    chain: sepolia,
+    transport: http(SEPOLIA_RPC_URL),
+  });
+
+  const contract = getContract({
+    address: gameAddress,
+    abi: contractABI,
+    client: client,
+  });
+
+  enum Enemy {
+    OLDMAN,
+    DEVIL,
+    ZOMBIE,
+  }
+
+  const [selectedEnemy, setSelectedEnemy] = useState(Enemy.OLDMAN);
 
   // normal,swing,stab for attackType
   const [attackType, setAttackType] = useState('normal');
@@ -28,6 +74,94 @@ export default function GamePage() {
   const [oldManHp, setOldManHP] = useState(100);
   const [zombieHP, setZombieHP] = useState(100);
   const [devilHP, setDevilHP] = useState(100);
+
+  const stab = async () => {
+    let enemy;
+    if (selectedEnemy == Enemy.OLDMAN) {
+      enemy = 1;
+    } else if (selectedEnemy == Enemy.DEVIL) {
+      enemy = 2;
+    } else {
+      enemy = 3;
+    }
+    if (!isSignedIn || accountId === undefined) {
+      throw new Error('Please sign in to stab');
+    }
+    const nearViemAccount = (await wallet.nearViemAccount(
+      accountId,
+    )) as LocalAccount;
+    const walletClient = createWalletClient({
+      account: nearViemAccount,
+      transport: http(),
+      chain: sepolia,
+    });
+
+    try {
+      const { request } = await publicClient.simulateContract({
+        account: nearViemAccount,
+        address: gameAddress,
+        abi: contractABI,
+        functionName: 'stab',
+        args: [BigInt(0), BigInt(enemy)],
+      });
+      if (selectedEnemy == Enemy.OLDMAN) {
+        setOldManHP((prev) => prev - 40);
+      } else if (selectedEnemy == Enemy.DEVIL) {
+        setDevilHP((prev) => prev - 40);
+      } else {
+        setZombieHP((prev) => prev - 40);
+      }
+      const result = await walletClient.writeContract(request);
+      console.log(result);
+    } catch (error: any) {
+      alert('Stab failed: ' + error.message);
+      console.error('Stab error:', error);
+    }
+  };
+
+  const swing = async () => {
+    let enemy;
+    if (selectedEnemy == Enemy.OLDMAN) {
+      enemy = 1;
+    } else if (selectedEnemy == Enemy.DEVIL) {
+      enemy = 2;
+    } else {
+      enemy = 3;
+    }
+    if (!isSignedIn || accountId === undefined) {
+      throw new Error('Please sign in to swing');
+    }
+    const nearViemAccount = (await wallet.nearViemAccount(
+      accountId,
+    )) as LocalAccount;
+    const walletClient = createWalletClient({
+      account: nearViemAccount,
+      transport: http(),
+      chain: sepolia,
+    });
+
+    try {
+      const { request } = await publicClient.simulateContract({
+        account: nearViemAccount,
+        address: gameAddress,
+        abi: contractABI,
+        functionName: 'swing',
+        args: [BigInt(0), BigInt(enemy)],
+      });
+      if (selectedEnemy == Enemy.OLDMAN) {
+        setOldManHP((prev) => prev - 40);
+      } else if (selectedEnemy == Enemy.DEVIL) {
+        setDevilHP((prev) => prev - 40);
+      } else {
+        setZombieHP((prev) => prev - 40);
+      }
+      const result = await walletClient.writeContract(request);
+      console.log(result);
+    } catch (error: any) {
+      alert('Swing failed: ' + error.message);
+      console.error('Swing error:', error);
+    }
+  };
 
   return (
     <div className="mx-[6%] mt-[3%] flex flex-col">
@@ -51,13 +185,13 @@ export default function GamePage() {
       </div>
       <div className="characterContainer flex justify-between pt-[10%]">
         <HumanConditionalRender attackType={attackType} />
-        {selectedEnemy === 'oldMan' ? (
+        {selectedEnemy === Enemy.OLDMAN ? (
           <Image
             className="border-[4px] border-black"
             src={oldmanEnemy}
             alt={''}
             onClick={() => {
-              setSelectedEnemy('oldMan');
+              setSelectedEnemy(Enemy.OLDMAN);
             }}
           ></Image>
         ) : (
@@ -65,18 +199,18 @@ export default function GamePage() {
             src={oldmanEnemy}
             alt={''}
             onClick={() => {
-              setSelectedEnemy('oldMan');
+              setSelectedEnemy(Enemy.OLDMAN);
             }}
           ></Image>
         )}
 
-        {selectedEnemy === 'devil' ? (
+        {selectedEnemy === Enemy.DEVIL ? (
           <Image
             className="border-[4px] border-black"
             src={devilEnemy}
             alt={''}
             onClick={() => {
-              setSelectedEnemy('devil');
+              setSelectedEnemy(Enemy.DEVIL);
             }}
           ></Image>
         ) : (
@@ -84,17 +218,17 @@ export default function GamePage() {
             src={devilEnemy}
             alt={''}
             onClick={() => {
-              setSelectedEnemy('devil');
+              setSelectedEnemy(Enemy.DEVIL);
             }}
           ></Image>
         )}
-        {selectedEnemy === 'zombie' ? (
+        {selectedEnemy === Enemy.ZOMBIE ? (
           <Image
             className="border-[4px] border-black"
             src={zombieEnemy}
             alt={''}
             onClick={() => {
-              setSelectedEnemy('zombie');
+              setSelectedEnemy(Enemy.ZOMBIE);
             }}
           ></Image>
         ) : (
@@ -102,7 +236,7 @@ export default function GamePage() {
             src={zombieEnemy}
             alt={''}
             onClick={() => {
-              setSelectedEnemy('zombie');
+              setSelectedEnemy(Enemy.ZOMBIE);
             }}
           ></Image>
         )}
@@ -117,6 +251,7 @@ export default function GamePage() {
               setAttackType('normal');
             }, 1000);
             setAttackType('stab');
+            stab();
           }}
         ></Image>
         <Image
@@ -128,6 +263,7 @@ export default function GamePage() {
               setAttackType('normal');
             }, 1000);
             setAttackType('swing');
+            swing();
           }}
         ></Image>
       </div>
